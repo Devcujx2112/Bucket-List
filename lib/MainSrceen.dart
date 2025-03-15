a22887f1f8fbddd3f3a14c4ebc3f310da436c842
Devcujx2112 <Vutungduongnight@gmail.com>
2025-03-15 23:42:20 +0700

    API

--- lib/MainSrceen.dart
+++ lib/MainSrceen.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,8 +10,70 @@ class Mainsrceen extends StatefulWidget {
 }
 
 class _MainsrceenState extends State<Mainsrceen> {
+  List<dynamic> listProduct = [];
+
+  Future<void> GetData() async {
+    try {
+      Response response = await Dio().get(
+          "https://flutter-api-2f232-default-rtdb.firebaseio.com/listproduct.json");
+      listProduct = response.data;
+      setState(() {});
+    } catch (e) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('Wrong URL'),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cancel'))
+              ],
+            );
+          });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Bucket List'),),);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Bucket List'),
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(padding: EdgeInsets.all(20),
+                  child: ElevatedButton(
+                      onPressed: GetData, child: Text('Get Data')),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text("Them san pham"))
+            ],),
+          Expanded(
+              child: ListView.builder(
+                itemCount: listProduct.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                      padding: EdgeInsets.all(10),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          radius: 40,
+                          backgroundImage: NetworkImage(
+                            listProduct[index]['image'] ?? "",
+                          ),
+                        ),
+                        title: Text(listProduct[index]["name"] ?? []),
+                        trailing: Text(listProduct[index]["item"] ?? []),
+                      ));
+                },
+              ))
+        ],
+      ),
+    );
   }
 }
